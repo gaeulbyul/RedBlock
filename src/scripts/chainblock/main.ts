27de25953f5b629506ade947dcbabf43f7bc9465
Gaeulbyul <aeiou256@gmail.com>
2018-10-17 12:29:33 +0000

    체인블락 정지 구현

--- src/scripts/chainblock/main.ts
+++ src/scripts/chainblock/main.ts
@@ -11,6 +11,7 @@ class ChainBlocker {
   constructor () {}
   async start (targetUserName: string) {
     const ui = this.ui
+    ui.show()
     const targetUser = await TwitterAPI.getSingleUserByName(targetUserName)
     ui.updateTarget(targetUser)
     const progress: ChainblockProgress = {
@@ -20,56 +21,53 @@ class ChainBlocker {
       blockSuccess: 0,
       blockFail: 0
     }
-    for await (const user of TwitterAPI.getAllFollowers(targetUserName)) {
-      if (user === 'RateLimitError') {
-        TwitterAPI.getRateLimitStatus().then((limits: LimitStatus) => {
-          const followerLimit = limits.followers['/followers/list']
-          ui.rateLimited(followerLimit)
-        })
-        const second = 1000
-        const minute = second * 60
-        await sleep(1 * minute)
-        continue
+    try {
+      for await (const user of TwitterAPI.getAllFollowers(targetUserName)) {
+        const shouldStop = [ChainBlockUIState.Closed, ChainBlockUIState.Stopped].includes(ui.state)
+        if (shouldStop) {
+          break
+        }
+        if (user === 'RateLimitError') {
+          TwitterAPI.getRateLimitStatus().then((limits: LimitStatus) => {
+            const followerLimit = limits.followers['/followers/list']
+            ui.rateLimited(followerLimit)
+          })
+          const second = 1000
+          const minute = second * 60
+          await sleep(1 * minute)
+          continue
+        }
+        ui.rateLimitResetted()
+        if (user.blocking) {
+          ++progress.alreadyBlocked
+          ui.updateProgress(Object.assign({}, progress))
+          continue
+        }
+        const followSkip = _.some([
+          user.following,
+          user.followed_by,
+          user.follow_request_sent
+        ])
+        if (followSkip) {
+          ++progress.skipped
+          ui.updateProgress(Object.assign({}, progress))
+          continue
+        }
+        const blockResult = await TwitterAPI.blockUser(user)
+        if (blockResult) {
+          ++progress.blockSuccess
+          ui.updateProgress(Object.assign({}, progress))
+        } else {
+          ++progress.blockFail
+          ui.updateProgress(Object.assign({}, progress))
+        }
+        await sleep(10)
       }
-      ui.rateLimitResetted()
-      if (user.blocking) {
-        ++progress.alreadyBlocked
-        ui.updateProgress(Object.assign({}, progress))
-        continue
-      }
-      const followSkip = _.some([
-        user.following,
-        user.followed_by,
-        user.follow_request_sent
-      ])
-      if (followSkip) {
-        ++progress.skipped
-        ui.updateProgress(Object.assign({}, progress))
-        continue
-      }
-      // console.warn('WARNING: fake block!')
-      // TODO: implement block, real block
-      const blockResult = await TwitterAPI.blockUser(user)
-      if (blockResult) {
-        ++progress.blockSuccess
-        ui.updateProgress(Object.assign({}, progress))
-      } else {
-        ++progress.blockFail
-        ui.updateProgress(Object.assign({}, progress))
-      }
-      await sleep(10)
+      ui.complete(Object.assign({}, progress))
+    } catch (err) {
+      const error = err as Error
+      ui.error(error.message)
+      throw err
     }
-    ui.finalize(Object.assign({}, progress))
   }
-}
-
-
-function shouldBlock(user: TwitterUser): boolean {
-  const reasons = [
-    user.blocking, // 이미 차단함
-    user.following, // 내가 팔로우중
-    user.followed_by, // 나를 팔로우함
-    user.follow_request_sent // (프로텍트 계정의 경우) 팔로우 신청 대기 중. (팔로우중으로 취급)
-  ]
-  return !(reasons.some(t => t))
 }
--- src/scripts/chainblock/ui.ts
+++ src/scripts/chainblock/ui.ts
@@ -2,7 +2,8 @@ const CHAINBLOCK_UI_HTML = `
   <div class="redblock-bg modal-container block-dialog" style="display:flex">
     <div class="redblock-dialog modal modal-content is-autoPosition">
       <div class="redblock-titlebar">
-        <span class="redblock-title">§체인블락</span>
+        <span class="redblock-title">체인블락</span>
+        (<span class="redblock-state"></span>)
       </div>
       <div class="redblock-progress-container">
         <progress class="redblock-progress"></progress>
@@ -30,37 +31,75 @@ const CHAINBLOCK_UI_HTML = `
   </div>
 `
 
+const enum ChainBlockUIState {
+  Initial,
+  Running,
+  RateLimited,
+  Completed,
+  Stopped,
+  Closed,
+  Error
+}
+
+// shortcut function to change element's textContent
+function setText (root: Element) {
+  return (query: string, text: string | number) => {
+    const elem = root.querySelector(query)
+    if (elem) {
+      elem.textContent = text.toString()
+    }
+  }
+}
+
 class ChainBlockUI {
-  private ui: HTMLElement
+  private rootElem: HTMLElement = document.createElement('div')
+  public state: ChainBlockUIState = ChainBlockUIState.Initial
   // public scraper: AsyncIterableIterator<TwitterUser>
   constructor () {
-    const ui = this.ui = document.createElement('div')
-    ui.innerHTML = CHAINBLOCK_UI_HTML
-    document.body.appendChild(ui)
+    this.rootElem.innerHTML = CHAINBLOCK_UI_HTML
+  }
+  public show () {
+    document.body.appendChild(this.rootElem)
     this.attachEvent()
+    this.updateState(ChainBlockUIState.Running)
   }
   public updateTarget (targetUser: TwitterUser) {
-    const ui = this.ui
-    ui.querySelector('.redblock-target-name')!.textContent = targetUser.name
-    ui.querySelector('.redblock-target-total-followers')!.textContent = targetUser.followers_count.toString()
-    ui.querySelector<HTMLProgressElement>('.redblock-progress')!.max = targetUser.followers_count
+    const rootElem = this.rootElem
+    const txt = setText(rootElem)
+    txt('.redblock-target-name', targetUser.name)
+    txt('.redblock-target-total-followers', targetUser.followers_count)
+    rootElem.querySelector<HTMLProgressElement>('.redblock-progress')!.max = targetUser.followers_count
   }
   public updateProgress (progress: ChainblockProgress) {
-    const ui = this.ui
-    ui.querySelector('.redblock-blocked-user')!.textContent = progress.blockSuccess.toString()
-    ui.querySelector('.redblock-already-blocked-user')!.textContent = progress.alreadyBlocked.toString()
-    ui.querySelector('.redblock-skipped-user')!.textContent = progress.skipped.toString()
-    ui.querySelector('.redblock-failed-user')!.textContent = progress.blockFail.toString()
+    const rootElem = this.rootElem
+    const txt = setText(rootElem)
+    txt('.redblock-blocked-user', progress.blockSuccess)
+    txt('.redblock-already-blocked-user', progress.alreadyBlocked)
+    txt('.redblock-skipped-user', progress.skipped)
+    txt('.redblock-failed-user', progress.blockFail)
     const progressBarValue = _.sum([
       progress.alreadyBlocked,
       progress.blockFail,
       progress.blockSuccess,
       progress.skipped
     ])
-    ui.querySelector<HTMLProgressElement>('.redblock-progress')!.value = progressBarValue
+    rootElem.querySelector<HTMLProgressElement>('.redblock-progress')!.value = progressBarValue
+  }
+  public updateState (state: ChainBlockUIState) {
+    this.state = state
+    const rootElem = this.rootElem
+    const message: {[key: number]: string} = {
+      [ChainBlockUIState.Completed]: '완료',
+      [ChainBlockUIState.Running]: '작동 중...',
+      [ChainBlockUIState.RateLimited]: '일시정지(리밋)',
+      [ChainBlockUIState.Stopped]: '정지',
+      [ChainBlockUIState.Error]: '오류 발생!'
+    }
+    setText(rootElem)('.redblock-state', message[state] || '')
   }
   public rateLimited (limit: Limit) {
-    const ui = this.ui
+    this.updateState(ChainBlockUIState.RateLimited)
+    const rootElem = this.rootElem
     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
     const formatter = new Intl.DateTimeFormat('ko-KR', {
       timeZone,
@@ -70,24 +109,40 @@ class ChainBlockUI {
     // 120000 = 1000 * 60 * 2 = 리밋상태에서 체인블락의 delay간격
     const datetime = new Date((limit.reset * 1000) + 120000)
     const dtstr = formatter.format(datetime)
-    ui.querySelector<HTMLElement>('.redblock-ratelimit')!.hidden = false
-    ui.querySelector('.redblock-ratelimit-reset')!.textContent = dtstr
+    rootElem.querySelector<HTMLElement>('.redblock-ratelimit')!.hidden = false
+    rootElem.querySelector('.redblock-ratelimit-reset')!.textContent = dtstr
   }
   public rateLimitResetted () {
-    this.ui.querySelector<HTMLElement>('.redblock-ratelimit')!.hidden = true
+    this.updateState(ChainBlockUIState.Running)
+    this.rootElem.querySelector<HTMLElement>('.redblock-ratelimit')!.hidden = true
   }
-  private attachEvent () {
-    this.ui.querySelector('.redblock-close')!.addEventListener('click', event => {
-      event.preventDefault()
-      this.cleanup()
-    })
-  }
-  finalize (progress: ChainblockProgress) {
+  public complete (progress: ChainblockProgress) {
+    const currentState = this.state
+    this.updateState(ChainBlockUIState.Completed)
     this.updateProgress(progress)
-    const msg = `체인블락 완료! 총 ${progress.blockSuccess}명의 사용자를 차단했습니다.`
+    const reason = currentState === ChainBlockUIState.Running ? '완료' : '중단'
+    const msg = `체인블락 ${reason}! 총 ${progress.blockSuccess}명의 사용자를 차단했습니다.`
     window.alert(msg)
   }
-  cleanup () {
-    this.ui.remove()
+  public error (msg: string) {
+    this.updateState(ChainBlockUIState.Error)
+    window.alert(`오류 발생!\n메시지: "${msg}"`)
+  }
+  public stop () {
+    this.updateState(ChainBlockUIState.Stopped)
+  }
+  public close () {
+    this.updateState(ChainBlockUIState.Closed)
+    this.rootElem.remove()
+  }
+  private attachEvent() {
+    this.rootElem.addEventListener('redblock::stop-chainblock', () => { })
+    this.rootElem.querySelector('.redblock-close')!.addEventListener('click', event => {
+      event.preventDefault()
+      const shouldNotClose = (this.state === ChainBlockUIState.Running && !window.confirm('체인블락을 중단할까요?'))
+      if (!shouldNotClose) {
+        this.close()
+      }
+    })
   }
 }
