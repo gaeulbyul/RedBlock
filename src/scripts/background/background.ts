1dbf9dd8e5c2363d7a83cfe067c458b44480799d
Gaeulbyul <aeiou256@gmail.com>
2019-12-17 01:36:17 +0900

    icon을 못 찾아서 알림이 안 뜨는 버그 고침

--- src/scripts/background/background.ts
+++ src/scripts/background/background.ts
@@ -1,7 +1,7 @@
 export function notify(message: string): void {
   const notif: browser.notifications.NotificationOptions = {
     type: 'basic',
-    iconUrl: 'icons/icon-128.png',
+    iconUrl: '/icons/icon-128.png',
     title: 'Red Block',
     message,
   }
