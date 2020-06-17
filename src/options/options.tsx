55532dba508d89a2f84ac59001c1b444e97b79ab
Gaeulbyul <aeiou256@gmail.com>
2020-06-18 08:06:29 +0900

    [WIP] 옵션 UI 작업
    - Material UI 적용
    - 단어목록 UI 작업 중

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,7 +1,70 @@
 import * as Storage from '../scripts/background/storage.js'
 import * as i18n from '../scripts/i18n.js'
 
+const optionsMuiTheme = MaterialUI.createMuiTheme({
+  palette: {
+    primary: MaterialUI.colors.pink,
+    secondary: MaterialUI.colors.indigo,
+  },
+})
+
+const M = MaterialUI
+const T = MaterialUI.Typography
+
 type RedBlockOptions = Storage.RedBlockStorage['options']
+
+const useStylesForTable = MaterialUI.makeStyles(_theme => ({
+  fullWidth: {
+    width: '100%',
+  },
+}))
+
+function BadwordsTable() {
+  // L10N-ME
+  const { Table, TableHead, TableBody, TableCell, TableContainer, TableRow, TableFooter } = MaterialUI
+  const classes = useStylesForTable()
+  return (
+    <M.Paper className={classes.fullWidth}>
+      <TableContainer>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell padding="checkbox">
+                <M.Checkbox>
+                </M.Checkbox>
+              </TableCell>
+              <TableCell>
+                <strong>단어</strong>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell padding="checkbox">
+                <M.Checkbox>
+                </M.Checkbox>
+              </TableCell>
+              <TableCell>
+                asdf
+              </TableCell>
+            </TableRow>
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell>
+                <M.Input placeholder="[여기에 대충 단어 입력]" />
+                <M.Button>
+                  추가하기
+                </M.Button>
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </M.Paper>
+  )
+}
 
 function OptionsApp() {
   const [options, setOptions] = React.useState<RedBlockOptions>(Storage.defaultOptions)
@@ -15,53 +78,50 @@ function OptionsApp() {
     setOptions(newOptions)
   }
   return (
-    <div>
-      <fieldset>
-        <legend>옵션 / Options</legend>
-        <div className="option-item">
-          <label className="field checkbox-field">
-            <input
-              type="checkbox"
-              checked={options.useStandardBlockAPI}
-              onChange={() =>
-                mutateOptions({
-                  useStandardBlockAPI: !options.useStandardBlockAPI,
-                })
-              }
-            />
-            <span>{i18n.getMessage('use_official_block_api')}</span>
-          </label>
-          <div className="info-option">
-            <p>{i18n.getMessage('use_official_block_api_description')}</p>
-            <div className="caution">
-              <p>{i18n.getMessage('use_official_block_api_warning')}</p>
-            </div>
-          </div>
-        </div>
-        <div className="option-item">
-          <label className="field checkbox-field">
-            <input
-              type="checkbox"
-              checked={options.removeSessionAfterComplete}
-              onChange={() =>
-                mutateOptions({
-                  removeSessionAfterComplete: !options.removeSessionAfterComplete,
-                })
-              }
-            />
-            <span>{i18n.getMessage('remove_session_after_complete')}</span>
-          </label>
-          <div className="info-option"></div>
-        </div>
-      </fieldset>
-      {/*
-      <fieldset>
-        <legend>실험적인 기능 / Experimental Features</legend>
-        <span>현재는 없습니다. / Not available at the moment.</span>
-        <div className="option-item"></div>
-      </fieldset>
-      */}
-    </div>
+    <M.ThemeProvider theme={optionsMuiTheme}>
+      <M.AppBar position="static">
+        <M.Toolbar variant="dense">
+          <T variant="h6">
+            옵션 / Options
+          </T>
+        </M.Toolbar>
+      </M.AppBar>
+      <M.Container maxWidth="md">
+        <M.Box padding="10px">
+          <M.FormControl component="fieldset" fullWidth>
+            <M.FormLabel component="legend">체인블락 / Chainblock</M.FormLabel>
+            <M.FormGroup>
+              <M.FormControlLabel
+                control={<M.Checkbox size="small" />}
+                onChange={() =>
+                  mutateOptions({
+                    useStandardBlockAPI: !options.useStandardBlockAPI,
+                  })
+                }
+                checked={options.useStandardBlockAPI}
+                label={i18n.getMessage('use_official_block_api')}
+              />
+              <M.FormHelperText>{i18n.getMessage('use_official_block_api_warning')}</M.FormHelperText>
+              <M.FormControlLabel
+                control={<M.Checkbox size="small" />}
+                onChange={() =>
+                  mutateOptions({
+                    removeSessionAfterComplete: !options.removeSessionAfterComplete,
+                  })
+                }
+                checked={options.removeSessionAfterComplete}
+                label={i18n.getMessage('remove_session_after_complete')}
+              />
+            </M.FormGroup>
+          </M.FormControl>
+          <M.Divider />
+          <M.FormControl component="fieldset" fullWidth>
+            <M.FormLabel component="legend">원클릭차단 / One-click block</M.FormLabel>
+            <BadwordsTable />
+          </M.FormControl>
+        </M.Box>
+      </M.Container>
+    </M.ThemeProvider>
   )
 }
 
