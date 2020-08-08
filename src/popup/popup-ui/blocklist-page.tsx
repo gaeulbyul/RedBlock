6a0b311e9120c73721b22c373d564819d21a520f
Gaeulbyul <aeiou256@gmail.com>
2020-08-08 02:41:14 +0000

    차단목록 가져오기 기능 개선
    - 파일선택한 시점에 parse하고
    - 중복 및 잘못된 값 갯수를 보여줌
    - 팝업에 로그인체크 UI 표시

--- src/popup/popup-ui/blocklist-page.tsx
+++ src/popup/popup-ui/blocklist-page.tsx
@@ -1,16 +1,19 @@
-﻿import { SnackBarContext } from './contexts.js'
+import { SnackBarContext, LoginStatusContext } from './contexts.js'
+import { PleaseLoginBox } from './ui-common.js'
 import { PageEnum } from '../popup.js'
 import * as i18n from '../../scripts/i18n.js'
 
-import { importBlocklist } from '../../scripts/background/blocklist-process.js'
+import { Blocklist, emptyBlocklist, importBlocklist, parse } from '../../scripts/background/blocklist-process.js'
 
 const M = MaterialUI
 const T = MaterialUI.Typography
 
 export default function BlocklistPage() {
+  const { loggedIn } = React.useContext(LoginStatusContext)
   const snackBarCtx = React.useContext(SnackBarContext)
   const [fileInput] = React.useState(React.createRef<HTMLInputElement>())
-  async function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+  const [blocklist, setBlocklist] = React.useState<Blocklist>(emptyBlocklist)
+  async function onChange(event: React.FormEvent<HTMLInputElement>) {
     event.preventDefault()
     const files = fileInput.current!.files
     if (!(files && files.length > 0)) {
@@ -19,7 +22,15 @@ export default function BlocklistPage() {
     }
     const file = files[0]
     const text = await file.text()
-    importBlocklist(text)
+    setBlocklist(parse(text))
+  }
+  async function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    if (blocklist.userIds.size <= 0) {
+      snackBarCtx.snack(i18n.getMessage('cant_chainblock_empty_list'))
+      return
+    }
+    importBlocklist(blocklist.userIds)
   }
   async function openPopupUIInTab(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     event.preventDefault()
@@ -30,42 +41,51 @@ export default function BlocklistPage() {
     window.close()
   }
   return (
-    <M.ExpansionPanel defaultExpanded>
-      <M.ExpansionPanelSummary>
-        <T>{i18n.getMessage('import_blocklist')}</T>
-      </M.ExpansionPanelSummary>
-      <M.ExpansionPanelDetails>
-        <div style={{ width: '100%' }}>
-          <form onSubmit={onSubmit}>
-            <M.FormControl component="fieldset" fullWidth>
-              <M.Box display="flex" flexDirection="row" alignItems="center" justifyContent="space-between">
-                <M.Box flexGrow="1">
-                  <input
-                    required
-                    ref={fileInput}
-                    id="input-file-to-import"
-                    name="input-file"
-                    type="file"
-                    accept="text/plain,text/csv,application/json,.CSV"
-                  />
+    <div>
+      <M.ExpansionPanel defaultExpanded>
+        <M.ExpansionPanelSummary>
+          <T>{i18n.getMessage('import_blocklist')}</T>
+        </M.ExpansionPanelSummary>
+        <M.ExpansionPanelDetails>
+          <div style={{ width: '100%' }}>
+            <form onSubmit={onSubmit}>
+              <M.FormControl component="fieldset" fullWidth>
+                <M.Box display="flex" flexDirection="row" alignItems="center" justifyContent="space-between">
+                  <M.Box flexGrow="1">
+                    <input
+                      required
+                      ref={fileInput}
+                      id="input-file-to-import"
+                      name="input-file"
+                      type="file"
+                      onChange={onChange}
+                      accept="text/plain,text/csv,application/json,.CSV"
+                    />
+                  </M.Box>
+                  <M.Button type="submit" variant="contained" color="primary" component="button" disabled={!loggedIn}>
+                    {i18n.getMessage('import')}
+                  </M.Button>
                 </M.Box>
-                <M.Button type="submit" variant="contained" color="primary" component="button">
-                  {i18n.getMessage('import')}
+              </M.FormControl>
+            </form>
+            <div className="description">
+              {/* L10N-ME */}
+              <p>
+                차단할 유저ID: <strong>{blocklist.userIds.size.toLocaleString()}</strong>개 / 중복:{' '}
+                {blocklist.duplicated.toLocaleString()} / 값이 잘못됨: {blocklist.invalid.toLocaleString()}
+              </p>
+              <p>{i18n.getMessage('blocklist_import_description')}</p>
+              <div className="hide-on-tab">
+                <p style={{ fontWeight: 'bold' }}>⚠ {i18n.getMessage('open_new_tab_for_file_picker')}</p>
+                <M.Button type="button" variant="outlined" component="button" onClick={openPopupUIInTab}>
+                  {i18n.getMessage('open_in_new_tab')}
                 </M.Button>
-              </M.Box>
-            </M.FormControl>
-          </form>
-          <div className="description">
-            <p>{i18n.getMessage('blocklist_import_description')}</p>
-            <div className="hide-on-tab">
-              <p style={{ fontWeight: 'bold' }}>⚠ {i18n.getMessage('open_new_tab_for_file_picker')}</p>
-              <M.Button type="button" variant="outlined" component="button" onClick={openPopupUIInTab}>
-                {i18n.getMessage('open_in_new_tab')}
-              </M.Button>
+              </div>
             </div>
           </div>
-        </div>
-      </M.ExpansionPanelDetails>
-    </M.ExpansionPanel>
+        </M.ExpansionPanelDetails>
+      </M.ExpansionPanel>
+      {loggedIn ? '' : <PleaseLoginBox />}
+    </div>
   )
 }
--- src/scripts/background/blocklist-process.ts
+++ src/scripts/background/blocklist-process.ts
@@ -1,52 +1,90 @@
 import { startImportChainBlock } from './request-sender.js'
 import { MAX_USER_LIMIT } from '../common.js'
 
+export interface Blocklist {
+  userIds: Set<string>
+  duplicated: number
+  invalid: number
+}
+
+export const emptyBlocklist: Blocklist = {
+  userIds: new Set(),
+  duplicated: 0,
+  invalid: 0,
+}
+
+/*
 function* iterateRegexp(pattern: RegExp, text: string) {
   let match: RegExpExecArray | null
   while ((match = pattern.exec(text))) {
     yield match[0]
   }
 }
+*/
 
-function parseAsImportChainJson(text: string): Set<string> {
+function parseAsImportChainJson(text: string): Blocklist {
   const json = JSON.parse(text)
   if (!Array.isArray(json.users)) {
     throw new Error('invalid file?')
   }
-  const userIds = new Set<string>()
   const importedJson = json as ImportChainJson
-  importedJson.users.forEach(({ id }, index) => {
-    if (typeof id !== 'string') {
-      throw new Error('invalid file?')
-    }
-    if (index < MAX_USER_LIMIT) {
-      userIds.add(id)
-    }
-  })
-  return userIds
-}
-
-function parseAsCsvBlocklist(text: string): Set<string> {
   const userIds = new Set<string>()
-  const iterator = iterateRegexp(/^\d+$/gm, text)
-  Array.from(iterator).forEach((userId, index) => {
-    if (index < MAX_USER_LIMIT) {
-      userIds.add(userId)
+  let duplicated = 0
+  let invalid = 0
+  for (const { id } of importedJson.users) {
+    if (!/^\d+$/.test(id)) {
+      invalid++
+      continue
     }
-  })
-  return userIds
+    if (userIds.has(id)) {
+      duplicated++
+      continue
+    }
+    userIds.add(id)
+    if (userIds.size >= MAX_USER_LIMIT) {
+      break
+    }
+  }
+  return { userIds, duplicated, invalid }
 }
 
-export async function importBlocklist(text: string) {
-  let userIds: Set<string>
-  if (text.startsWith('{')) {
-    userIds = parseAsImportChainJson(text)
-  } else {
-    userIds = parseAsCsvBlocklist(text)
+function parseAsCsvBlocklist(text: string): Blocklist {
+  const splitted = text.split('\n')
+  const userIds = new Set<string>()
+  let duplicated = 0
+  let invalid = 0
+  for (const line_ of splitted) {
+    const line = line_.trim()
+    if (line === '') {
+      continue
+    }
+    if (!/^\d+$/.test(line)) {
+      invalid++
+      continue
+    }
+    if (userIds.has(line)) {
+      duplicated++
+      continue
+    }
+    userIds.add(line)
+    if (userIds.size >= MAX_USER_LIMIT) {
+      break
+    }
   }
-  const isValid = userIds && userIds.size > 0
-  if (!isValid) {
-    throw new Error('error to parse blocklist')
+  return { userIds, duplicated, invalid }
+}
+
+export function parse(text: string): Blocklist {
+  if (text.startsWith('{')) {
+    return parseAsImportChainJson(text)
+  } else {
+    return parseAsCsvBlocklist(text)
+  }
+}
+
+export async function importBlocklist(userIds: Set<string>) {
+  if (userIds.size <= 0) {
+    throw new Error('empty file')
   }
   startImportChainBlock({
     purpose: 'chainblock',
