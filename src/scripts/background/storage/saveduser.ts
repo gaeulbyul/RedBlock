16f4f4f9de1770f853ac197c9d88a21b289c175c
Gaeulbyul <aeiou256@gmail.com>
2021-01-16 16:42:05 +0900

    민감한 데이터가 저장되지 않도록 처리함
    자신의 .email과 .phone속성은 저장이나 불러올 때 제거함

--- src/scripts/background/storage/saveduser.ts
+++ src/scripts/background/storage/saveduser.ts
@@ -5,14 +5,14 @@ export async function loadUsers(): Promise<TwitterUserMap> {
     'savedUsers'
   )) as unknown) as RedBlockStorage
   if (savedUsers) {
-    return TwitterUserMap.fromUsersArray(savedUsers)
+    return TwitterUserMap.fromUsersArray(savedUsers.map(deleteSensitiveInfo))
   } else {
     return new TwitterUserMap()
   }
 }
 
 export async function saveUsers(usersMap: TwitterUserMap): Promise<void> {
-  const savedUsers: RedBlockStorage['savedUsers'] = usersMap.toUserArray()
+  const savedUsers: RedBlockStorage['savedUsers'] = usersMap.toUserArray().map(deleteSensitiveInfo)
   const storageObject = { savedUsers }
   return browser.storage.local.set(storageObject as any)
 }
@@ -27,4 +27,15 @@ export async function removeUser(user: TwitterUser): Promise<void> {
   const users = await loadUsers()
   users.delete(user.id_str)
   return saveUsers(users)
+}
+
+function deleteSensitiveInfo(user: TwitterUser): TwitterUser {
+  const userAsAny = user as any
+  if (Object.isFrozen(userAsAny)) {
+    const clonedUser = Object.assign({}, userAsAny)
+    return deleteSensitiveInfo(clonedUser)
+  }
+  delete userAsAny.email
+  delete userAsAny.phone
+  return user
 }
