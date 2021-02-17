b262df384155b9518a25ee3a4ac7ba1902ccc482
Gaeulbyul <aeiou256@gmail.com>
2021-02-18 01:47:53 +0900

    export 세션에 AntiBlock 구현

--- src/scripts/background/chainblock-session/userid-scraper.ts
+++ src/scripts/background/chainblock-session/userid-scraper.ts
@@ -1,5 +1,7 @@
 import { TwClient } from '../twitter-api.js'
+import * as CookieHandler from '../cookie-handler.js'
 import * as UserScrapingAPI from '../user-scraping-api.js'
+import * as i18n from '../../i18n.js'
 import { getFollowersCount, getReactionsCount, wrapEitherRight } from '../../common.js'
 
 export interface UserIdScraper {
@@ -32,6 +34,90 @@ class MutualFollowerScraper implements UserIdScraper {
     )
     this.totalCount = mutualFollowersIds.length
     yield wrapEitherRight({ ids: mutualFollowersIds })
+  }
+}
+
+// 차단상대 대상 스크래퍼
+class AntiBlockScraper implements UserIdScraper {
+  public totalCount: number | null = null
+  public constructor(private request: FollowerBlockSessionRequest) {}
+  private async *getMutualFollowersIdsWith(
+    secondaryScrapingClient: UserScrapingAPI.UserScrapingAPIClient
+  ) {
+    const mutualFollowerIds = await secondaryScrapingClient.getAllMutualFollowersIds(
+      this.request.target.user
+    )
+    yield wrapEitherRight({ ids: mutualFollowerIds })
+  }
+  private async *getFollowersIdsWith(
+    secondaryScrapingClient: UserScrapingAPI.UserScrapingAPIClient
+  ) {
+    const { user, list: followKind } = this.request.target
+    yield* secondaryScrapingClient.getAllFollowsIds(followKind, user)
+  }
+  private async prepareActor(): Promise<TwClient | null> {
+    const targetUserId = this.request.target.user.id_str
+    const cookieStores = await browser.cookies.getAllCookieStores()
+    for (const store of cookieStores) {
+      const cookieStoreId = store.id
+      // 컨테이너에 트위터 계정을 하나만 로그인한 경우, auth_multi 쿠키가 없어 서
+      // getMultiAccountCookies 함수가 null 을 리턴한다.
+      const secondaryTwClients: TwClient[] = [new TwClient({ cookieStoreId })]
+      const multiCookies = await CookieHandler.getMultiAccountCookies({ cookieStoreId })
+      if (multiCookies) {
+        const actorUserIds = Object.keys(multiCookies)
+        for (const actAsUserId of actorUserIds) {
+          secondaryTwClients.push(
+            new TwClient({
+              cookieStoreId,
+              actAsUserId,
+            })
+          )
+        }
+      }
+      console.debug(
+        '[AntiBlock]: storeId: "%s" multiCookies:%o clients:%o',
+        cookieStoreId,
+        multiCookies,
+        secondaryTwClients
+      )
+      for (const secondaryTwClient of secondaryTwClients) {
+        console.debug('[AntiBlock]: secondaryTwClient:%o', secondaryTwClient)
+        const secondaryMyself = await secondaryTwClient.getMyself().catch(() => null)
+        if (!secondaryMyself) {
+          console.debug('[AntiBlock]: login check failed')
+          continue
+        }
+        if (secondaryMyself.id_str === this.request.myself.id_str) {
+          continue
+        }
+        const target = await secondaryTwClient
+          .getSingleUser({ user_id: targetUserId })
+          .catch(() => null)
+        if (target && !target.blocked_by) {
+          console.debug('[AntiBlock]: Found! will use %o', secondaryTwClient)
+          return secondaryTwClient
+        }
+      }
+    }
+    return null
+  }
+  private async *fetchFollowersIds(secondaryScrapingClient: UserScrapingAPI.UserScrapingAPIClient) {
+    const { user, list: followKind } = this.request.target
+    if (followKind === 'mutual-followers') {
+      yield* this.getMutualFollowersIdsWith(secondaryScrapingClient)
+    } else {
+      this.totalCount = getFollowersCount(user, followKind)!
+      yield* this.getFollowersIdsWith(secondaryScrapingClient)
+    }
+  }
+  public async *[Symbol.asyncIterator]() {
+    const secondaryTwClient = await this.prepareActor()
+    if (!secondaryTwClient) {
+      throw new Error(i18n.getMessage('cant_chainblock_to_blocked'))
+    }
+    const secondaryScrapingClient = new UserScrapingAPI.UserScrapingAPIClient(secondaryTwClient)
+    yield* this.fetchFollowersIds(secondaryScrapingClient)
   }
 }
 
@@ -82,6 +168,9 @@ export function initIdScraper(
   const { target } = request
   if (target.type === 'tweet_reaction') {
     return new TweetReactedUserScraper(twClient, request as TweetReactionBlockSessionRequest)
+  }
+  if (target.user.blocked_by) {
+    return new AntiBlockScraper(request as FollowerBlockSessionRequest)
   }
   if (target.list === 'mutual-followers') {
     return new MutualFollowerScraper(twClient, request as FollowerBlockSessionRequest)
