ff82cab58268deb832edd7fed3b0c63d70410714
Gaeulbyul <aeiou256@gmail.com>
2021-04-06 16:52:12 +0900

    swc 업데이트 및 빌드스크립트 수정
    * swc 버그해결한 버전 (^v1.2.50) 적용
    * swc의 -D 옵션을 사용하고, ncp 패키지 제거

--- just.config.js
+++ just.config.js
@@ -9,7 +9,6 @@ const manifest = require('./src/manifest.json')
 const name = manifest.name.replace(/[^\w\-]+/gi, '')
 const version = manifest.version
 
-const cp = util.promisify(ncp)
 const rmrf = util.promisify(rimraf)
 const exec = util.promisify(proc.exec)
 
@@ -17,21 +16,12 @@ task('check-tsc', async () => {
   await exec('tsc --noEmit')
 })
 
-task('build-assets', async () => {
-  await cp('src/', 'build/', {
-    stopOnErr: true,
-    filter(filename) {
-      return !/\.tsx?$/.test(filename)
-    },
-  })
-})
-
 task('build-tsc', async () => {
   await exec('tsc')
 })
 
 task('build-swc', async () => {
-  await exec('swc --out-dir=build/ src/')
+  await exec('swc -D --out-dir=build/ src/')
 })
 
 task('clean', async () => {
@@ -50,7 +40,7 @@ task('srczip', async () => {
   await exec(`git archive -9 -v -o ./dist/${name}-v${version}.Source.zip HEAD`)
 })
 
-task('build', parallel('build-assets', 'build-swc', 'check-tsc'))
+task('build', parallel('build-swc', 'check-tsc'))
 task('default', series('clean', 'build'))
 task('dist', parallel('zip', 'srczip'))
 task('all', series('default', 'dist'))
