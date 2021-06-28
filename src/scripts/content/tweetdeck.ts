7623172b0074d37e0130eed2ebdbaf977f9f3381
Gaeulbyul <aeiou256@gmail.com>
2021-06-28 20:44:25 +0900

    파일경로 수정

--- src/scripts/content/tweetdeck.ts
+++ src/scripts/content/tweetdeck.ts
@@ -2,4 +2,4 @@ import { listenExtensionMessages, injectScriptToPage } from './content-common'
 
 listenExtensionMessages(null)
 
-injectScriptToPage('bundled/tweetdeck-inject.bun.js')
+injectScriptToPage('bundled/tweetdeck_inject.bun.js')
--- src/scripts/content/twitter.ts
+++ src/scripts/content/twitter.ts
@@ -4,5 +4,5 @@ const reactRoot = document.getElementById('react-root')
 listenExtensionMessages(reactRoot)
 
 if (reactRoot) {
-  injectScriptToPage('bundled/twitter-inject.bun.js')
+  injectScriptToPage('bundled/twitter_inject.bun.js')
 }
