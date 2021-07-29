31efbc5b0e64d1d110b213de79ea74934e5b6cdd
Gaeulbyul <aeiou256@gmail.com>
2021-07-29 10:15:21 +0900

    옵션값 옮기기
    (antiblock=>blockbuster)

--- src/scripts/background/storage/options.ts
+++ src/scripts/background/storage/options.ts
@@ -1,26 +1,24 @@
-function deleteUnusedOptions(options: RedBlockOptions | null) {
+function migrateOptions(options: any) {
   // 최초 설치 후 실행시 null/undefined가 온다.
   if (!options) {
     return
   }
-  const optionsAsAny = options as any
-  delete optionsAsAny.tweetReactionBasedChainBlock
-  delete optionsAsAny.experimental_tweetReactionBasedChainBlock
-  delete optionsAsAny.enableRailgun
-  delete optionsAsAny.useStandardBlockAPI
-  delete optionsAsAny.enableChainMute
-  delete optionsAsAny.throttleBlockRequest
+  const redblockOptions = options as RedBlockOptions
+  if ('enableAntiBlock' in options) {
+    redblockOptions.enableBlockBuster = options.enableAntiBlock
+    delete options.enableAntiBlock
+  }
 }
 
 export async function loadOptions(): Promise<RedBlockOptions> {
   const { options } = (await browser.storage.local.get('options')) as unknown as RedBlockStorage
-  deleteUnusedOptions(options)
+  migrateOptions(options)
   return Object.assign({}, defaultOptions, options)
 }
 
 export async function saveOptions(newOptions: RedBlockOptions): Promise<void> {
   const options: RedBlockOptions = Object.assign({}, defaultOptions, newOptions)
-  deleteUnusedOptions(options)
+  migrateOptions(options)
   const storageObject = { options }
   return browser.storage.local.set(storageObject as any)
 }
