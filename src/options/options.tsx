b09884521cc0c5f506ab4e0b9d1e8e41423d6aa5
Gaeulbyul <aeiou256@gmail.com>
2021-08-01 22:36:16 +0900

    설정파일 백업/복원 UI 옮김 등

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -4,7 +4,6 @@ import * as MaterialUI from '@material-ui/core'
 import throttle from 'lodash-es/throttle'
 
 import * as Storage from '../scripts/background/storage'
-import { validateStorage } from '../scripts/background/storage/validator'
 import { TabPanel } from '../popup/popup-ui/components'
 import { RedBlockOptionsContext } from './pages/contexts'
 import ChainBlockOptionsPage from './pages/chainblock'
@@ -12,7 +11,6 @@ import OneClickBlockOptionsPage from './pages/oneclickblock'
 import ExperimentalOptionsPage from './pages/experimentals'
 import UserInterfaceOptionsPage from './pages/userinterface'
 import TroubleShootingsPage from './pages/troubleshooting'
-import * as i18n from '~~/scripts/i18n'
 
 const M = MaterialUI
 
@@ -36,75 +34,6 @@ function RedBlockOptionsUITheme(darkMode: boolean) {
       secondary: darkMode ? MaterialUI.colors.lightBlue : MaterialUI.colors.indigo,
     },
   })
-}
-
-function OptionsBackupUI() {
-  const [fileInput] = React.useState(React.createRef<HTMLInputElement>())
-  async function exportOptions() {
-    const storage = await browser.storage.local.get()
-    const manifest = browser.runtime.getManifest()
-    const filename = `${manifest.name}-Settings-v${manifest.version}.json`
-    const json = JSON.stringify(storage, null, '\t')
-    const jsonFile = new File([json], filename, { type: 'application/json' })
-    const objectUrl = URL.createObjectURL(jsonFile)
-    await browser.downloads
-      .download({
-        url: objectUrl,
-        filename,
-      })
-      .finally(() => {
-        URL.revokeObjectURL(objectUrl)
-      })
-  }
-  async function importOptions(event: React.FormEvent<HTMLInputElement>) {
-    event.preventDefault()
-    try {
-      const files = fileInput.current!.files
-      if (!(files && files.length > 0)) {
-        window.alert(i18n.getMessage('pick_file_first'))
-        return
-      }
-      const file = files[0]
-      const text = await file.text()
-      const json = JSON.parse(text)
-      const rbStorage = validateStorage(json)
-      // @ts-ignore
-      await browser.storage.local.set(rbStorage)
-    } catch (err: unknown) {
-      let errorMessage = ''
-      if (err instanceof Error) {
-        errorMessage = `${err.name}: ${err.message}`
-      }
-      window.alert('Error detected in file!\n' + errorMessage)
-    }
-  }
-  return (
-    <M.Box mx={1} my={2}>
-      <M.Paper>
-        <M.Box p={2}>
-          <form>
-            <input
-              ref={fileInput}
-              id="input-file-to-import"
-              name="input-file-to-import"
-              type="file"
-              onChange={importOptions}
-              style={{ display: 'none' }}
-              accept="application/json,.json"
-            />
-            <label htmlFor="input-file-to-import">
-              <M.Button variant="contained" component="span">
-                {i18n.getMessage('select_files')}
-              </M.Button>
-            </label>
-            <M.Button variant="contained" onClick={exportOptions}>
-              BACKUP
-            </M.Button>
-          </form>
-        </M.Box>
-      </M.Paper>
-    </M.Box>
-  )
 }
 
 function OptionsApp() {
@@ -167,8 +96,6 @@ function OptionsApp() {
             <ExperimentalOptionsPage />
           </TabPanel>
           <M.Divider />
-
-          <OptionsBackupUI />
         </M.Container>
       </RedBlockOptionsContext.Provider>
     </M.ThemeProvider>
--- src/options/pages/troubleshooting.tsx
+++ src/options/pages/troubleshooting.tsx
@@ -9,6 +9,7 @@ import {
   deleteTwitterCookies,
   nukeRedBlockSettings,
 } from '../../scripts/background/misc'
+import { validateStorage } from '../../scripts/background/storage/validator'
 
 function checkFirstPartyIsolationSupport() {
   try {
@@ -25,10 +26,85 @@ function checkFirstPartyIsolationSupport() {
 const M = MaterialUI
 const T = MaterialUI.Typography
 
+function OptionsBackupUI() {
+  const [fileInput] = React.useState(React.createRef<HTMLInputElement>())
+  async function exportOptions() {
+    const storage = await browser.storage.local.get()
+    const manifest = browser.runtime.getManifest()
+    const filename = `${manifest.name}-Settings-v${manifest.version}.json`
+    const json = JSON.stringify(storage, null, '\t')
+    const jsonFile = new File([json], filename, { type: 'application/json' })
+    const objectUrl = URL.createObjectURL(jsonFile)
+    await browser.downloads
+      .download({
+        url: objectUrl,
+        filename,
+      })
+      .finally(() => {
+        URL.revokeObjectURL(objectUrl)
+      })
+  }
+  async function importOptions(event: React.FormEvent<HTMLInputElement>) {
+    event.preventDefault()
+    try {
+      const files = fileInput.current!.files
+      if (!(files && files.length > 0)) {
+        window.alert(i18n.getMessage('pick_file_first'))
+        return
+      }
+      const file = files[0]
+      const text = await file.text()
+      const json = JSON.parse(text)
+      const rbStorage = validateStorage(json)
+      // @ts-ignore
+      await browser.storage.local.set(rbStorage)
+    } catch (err: unknown) {
+      let errorMessage = ''
+      if (err instanceof Error) {
+        errorMessage = `${err.name}: ${err.message}`
+      }
+      window.alert('Error detected in file!\n' + errorMessage)
+    }
+  }
+  return (
+    <M.Paper>
+      <M.Box p={2}>
+        <M.FormControl component="fieldset" fullWidth>
+          <M.FormLabel component="legend">백업 및 복원 / Backup &amp; Restore</M.FormLabel>
+          <input
+            ref={fileInput}
+            id="input-file-to-import"
+            name="input-file-to-import"
+            type="file"
+            onChange={importOptions}
+            style={{ display: 'none' }}
+            accept="application/json,.json"
+          />
+          <M.Divider />
+          <M.Box my={1} display="flex">
+            <M.Button
+              variant="contained"
+              onClick={exportOptions}
+              startIcon={<M.Icon>backup</M.Icon>}
+            >
+              {i18n.getMessage('redblock_backup_settings')}
+            </M.Button>
+            <M.Divider orientation="vertical" flexItem />
+            <label htmlFor="input-file-to-import">
+              <M.Button variant="contained" component="span" startIcon={<M.Icon>restore</M.Icon>}>
+                {i18n.getMessage('redblock_restore_settings')}
+              </M.Button>
+            </label>
+          </M.Box>
+        </M.FormControl>
+      </M.Box>
+    </M.Paper>
+  )
+}
+
 export default function TroubleShootingsPage() {
   const { options, updateOptions } = React.useContext(RedBlockOptionsContext)
   const firstPartyIsolatableBrowser = checkFirstPartyIsolationSupport()
-  const [resetCompleted, setResetCompleted] = React.useState(false)
   function confirmCookieDeletion() {
     const ok = window.confirm(i18n.getMessage('confirm_delete_cookie'))
     if (!ok) {
@@ -46,65 +122,73 @@ export default function TroubleShootingsPage() {
       return
     }
     nukeRedBlockSettings()
-    setResetCompleted(true)
+    location.reload()
   }
   return (
-    <M.Paper>
-      <M.Box padding="10px" margin="10px">
-        <M.FormControl component="fieldset" fullWidth>
-          <M.FormLabel component="legend">문제해결 / Troubleshooting</M.FormLabel>
-          <M.Divider />
-          <M.FormGroup>
-            <SwitchItem
-              checked={options.firstPartyIsolationCompatibleMode}
-              disabled={!firstPartyIsolatableBrowser}
-              label={i18n.getMessage('1st_party_isolation_compatible_mode')}
-              onChange={checked =>
-                updateOptions({
-                  firstPartyIsolationCompatibleMode: checked,
-                })
-              }
-            />
-          </M.FormGroup>
-          <M.FormHelperText>
-            {i18n.getMessage('1st_party_isolation_compatible_mode_description')}
-          </M.FormHelperText>
-          <M.Divider />
-          <M.FormGroup>
-            <M.FormControl component="fieldset">
-              <M.FormLabel>
-                <T>{i18n.getMessage('delete_cookie')}</T>
-              </M.FormLabel>
-              <div>
-                <M.Button variant="outlined" onClick={confirmCookieDeletion}>
-                  <span>{i18n.getMessage('delete_cookie')}</span>
-                </M.Button>
-              </div>
-            </M.FormControl>
-            <M.FormHelperText>{i18n.getMessage('delete_cookie_description')}</M.FormHelperText>
-          </M.FormGroup>
-          <M.Divider />
-          <M.FormGroup>
-            <M.FormControl component="fieldset">
-              <M.FormLabel>
-                <T>{i18n.getMessage('redblock_factory_reset')}</T>
-              </M.FormLabel>
-              <div>
-                <M.Button
-                  variant="outlined"
-                  onClick={confirmFactoryReset}
-                  disabled={resetCompleted}
-                >
-                  <span>RESET</span>
-                </M.Button>
-              </div>
-            </M.FormControl>
+    <React.Fragment>
+      <M.Paper>
+        <M.Box padding="10px" margin="10px">
+          <M.FormControl component="fieldset" fullWidth>
+            <M.FormLabel component="legend">문제해결 / Troubleshooting</M.FormLabel>
+            <M.Divider />
+            <M.FormGroup>
+              <SwitchItem
+                checked={options.firstPartyIsolationCompatibleMode}
+                disabled={!firstPartyIsolatableBrowser}
+                label={i18n.getMessage('1st_party_isolation_compatible_mode')}
+                onChange={checked =>
+                  updateOptions({
+                    firstPartyIsolationCompatibleMode: checked,
+                  })
+                }
+              />
+            </M.FormGroup>
             <M.FormHelperText>
-              {i18n.getMessage('redblock_factory_reset_description')}
+              {i18n.getMessage('1st_party_isolation_compatible_mode_description')}
             </M.FormHelperText>
-          </M.FormGroup>
-        </M.FormControl>
-      </M.Box>
-    </M.Paper>
+            <M.Divider />
+            <M.FormGroup>
+              <M.FormControl component="fieldset">
+                <M.FormLabel>
+                  <T>{i18n.getMessage('delete_cookie')}</T>
+                </M.FormLabel>
+                <div>
+                  <M.Button
+                    variant="contained"
+                    onClick={confirmCookieDeletion}
+                    startIcon={<M.Icon>delete_outline</M.Icon>}
+                  >
+                    <span>{i18n.getMessage('delete_cookie')}</span>
+                  </M.Button>
+                </div>
+              </M.FormControl>
+              <M.FormHelperText>{i18n.getMessage('delete_cookie_description')}</M.FormHelperText>
+            </M.FormGroup>
+            <M.Divider />
+            <M.FormGroup>
+              <M.FormControl component="fieldset">
+                <M.FormLabel>
+                  <T color="error">{i18n.getMessage('redblock_factory_reset')}</T>
+                </M.FormLabel>
+                <div>
+                  <M.Button
+                    variant="contained"
+                    onClick={confirmFactoryReset}
+                    color="primary"
+                    startIcon={<M.Icon>error_outline</M.Icon>}
+                  >
+                    <span>RESET</span>
+                  </M.Button>
+                </div>
+              </M.FormControl>
+              <M.FormHelperText error={true}>
+                {i18n.getMessage('redblock_factory_reset_description')}
+              </M.FormHelperText>
+            </M.FormGroup>
+          </M.FormControl>
+        </M.Box>
+      </M.Paper>
+      <OptionsBackupUI />
+    </React.Fragment>
   )
 }
