c66487e90b239f12eb62919d886b45844b665ed4
Gaeulbyul <aeiou256@gmail.com>
2022-04-05 21:28:17 +0900

    TwitterUserProfile에서 MUI 컴포넌트를 활용하도록 수정

--- src/popup/popup-ui/components.tsx
+++ src/popup/popup-ui/components.tsx
@@ -1,5 +1,5 @@
 import * as MaterialUI from '@mui/material'
-import { withStyles } from '@mui/styles'
+import { useTheme, withStyles } from '@mui/styles'
 
 import React from 'react'
 
@@ -298,35 +298,37 @@ export function TwitterUserProfile({
   user: TwitterUser | UserWithOnlyNameAndProfileImages
   children?: React.ReactNode
 }) {
+  const theme = useTheme()
+  // @ts-ignore 잘 되는데 왜 타입오류가 발생하지??
+  const textSecondary = theme.palette.text.secondary
   const biggerProfileImageUrl = user.profile_image_url_https.replace('_normal', '_bigger')
   return (
-    <div className="target-user-info">
-      <div className="profile-image-area">
-        <img
+    <M.Box display="flex" flexDirection="row" mt={1}>
+      <M.Box mr={1}>
+        <M.Avatar
           alt={i18n.getMessage('profile_image')}
-          className="profile-image"
           src={biggerProfileImageUrl}
+          sx={{ width: 64, height: 64 }}
         />
-      </div>
-      <div className="profile-right-area">
-        <div className="profile-right-info">
-          <div className="nickname" title={user.name}>
+      </M.Box>
+      <M.Box width="100%" lineHeight="150%">
+        <M.Box>
+          <T sx={{ fontWeight: 'bold', fontSize: 'larger' }}>
             {user.name}
-          </div>
-          <div className="username">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={`https://twitter.com/${user.screen_name}`}
-              title={i18n.getMessage('go_to_url', `https://twitter.com/${user.screen_name}`)}
-            >
-              @{user.screen_name}
-            </a>
-          </div>
-        </div>
+          </T>
+          <M.Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href={`https://twitter.com/${user.screen_name}`}
+            title={i18n.getMessage('go_to_url', `https://twitter.com/${user.screen_name}`)}
+            sx={{ color: 'text.secondary', textDecorationColor: textSecondary }}
+          >
+            @{user.screen_name}
+          </M.Link>
+        </M.Box>
         <div style={{ margin: '5px 0' }}>{children}</div>
-      </div>
-    </div>
+      </M.Box>
+    </M.Box>
   )
 }
 
