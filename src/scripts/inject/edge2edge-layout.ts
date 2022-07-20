51f38ecab19aff30c9d9b0556e82b6d75451fdcf
Gaeulbyul <aeiou256@gmail.com>
2022-07-20 23:01:22 +0900

    Edge2Edge 감지기능 고침

--- src/scripts/inject/edge2edge-layout.ts
+++ src/scripts/inject/edge2edge-layout.ts
@@ -3,9 +3,9 @@ import { findReduxStore } from './reactredux'
 export function detectEdge2EdgeLayoutSwitch() {
   const reduxStore = findReduxStore()
   try {
-    return reduxStore.getState().featureSwitch.user.config.media_edge_to_edge_content_enabled
+    return reduxStore.getState().featureSwitch.user.config.media_edge_to_edge_content_enabled.value
   } catch {
-    return true
+    return false
   }
 }
 
